3d4de6fc5afd7bf8d6fdd24e75a3e3f54d80bf82
Sri Ram Eswaran <111946836+sriramecs@users.noreply.github.com>
2023-05-08 13:52:20 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
-# Intelligent-Bookmarker
+# website-categorizer
 
 For running lda_api.py please make sure the following libraries are already installed
 
 ###
-pip install flask flask-cors scikit-learn
+pip install flask flask-cors scikit-learn transformers
 ###
 
-Then run python lda_api.py
+Then run python app.py
 
 Then load the extension by using the following approach:
 
 1. Open Chrome and navigate to chrome://extensions.
 2. Enable "Developer mode" by toggling the switch in the top right corner.
-3. Click "Load unpacked" and select the folder containing the above files .
+3. Click "Load unpacked" and select the website_categorizer folder you created earlier.
+
+If while loading extension you get an error remove __pycahce__ folder from the directory and try to load the extension again
--- lda_api.py
+++ lda_api.py
@@ -1,42 +1,7 @@
-import json
-import numpy as np
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-from sklearn.datasets import fetch_20newsgroups
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.decomposition import LatentDirichletAllocation
+from lda_gensim import GensimLDAClassifier
 
-app = Flask(__name__)
-CORS(app)
+classifier = GensimLDAClassifier()
 
-n_topics = 20
-n_top_words = 10
-
-# Load dataset
-newsgroups = fetch_20newsgroups(subset='all', remove=('headers', 'footers', 'quotes'))
-vectorizer = CountVectorizer(max_df=0.95, min_df=2, stop_words='english')
-X = vectorizer.fit_transform(newsgroups.data)
-
-# Train LDA model
-lda = LatentDirichletAllocation(n_components=n_topics, random_state=42)
-lda.fit(X)
-
-@app.route('/categorize', methods=['POST'])
-def categorize():
-    content = request.json['content']
-    X_new = vectorizer.transform([content])
-    topic_distribution = lda.transform(X_new)
-
-    topic_idx = np.argmax(topic_distribution)
-    topic_words = lda.components_[topic_idx]
-    top_word_indices = topic_words.argsort()[:-n_top_words - 1:-1]
-    top_words = [vectorizer.get_feature_names_out()[i] for i in top_word_indices]
-
-    response = {
-        'category': f'Topic {topic_idx + 1}',
-        'keywords': top_words
-    }
-    return jsonify(response)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def categorize_website(url):
+    topic_label = classifier.categorize_website(url)
+    return topic_label
